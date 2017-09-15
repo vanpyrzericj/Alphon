c3f94e1a06a23f14c627f33f5d947b927cb00ade
Eric J. Van Pyrz <Evanpyrz@jetmedia.us>
2017-09-15 14:23:45 -0400

    Updated configuration file to contain connection string. Also created Database context class and Account class. HomeController also has some basic CRUD commands in there.

---
+++ WebApp/WebApp/Models/HubContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class HubContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder
+                .UseMySql(@"Server=albatross.jm-corp.net;database=buhavore_buh;uid=buhavore_buh;pwd=GlCKGv_Pve3[;");
+        public DbSet<Account> Accounts { get; set; }
+    }
+}
