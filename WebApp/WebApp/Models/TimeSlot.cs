38d87d3677a8aff72a724850427bb1b591175f77
jf <jsfedyna@buffalo.edu>
2017-09-21 19:48:13 -0400

    Creation of base model classes

---
+++ WebApp/WebApp/Models/TimeSlot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class TimeSlot
+    {
+        public int Id { get; set; }
+        public string dayofweek { get; set; }
+        public DateTime starttime { get; set; }
+        public DateTime endtime { get; set; }
+
+    }
+}
