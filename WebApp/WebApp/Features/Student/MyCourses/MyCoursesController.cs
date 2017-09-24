a9f89be06ebf74befb7aca9bb63b2c52dd9176da
jf <jsfedyna@buffalo.edu>
2017-09-23 21:17:50 -0400

    Additions: Prototype student dashboard ui Prototype student courses ui
    Changes:
    Student sidebar link to dashboard
    Student enrollment ui text

---
+++ WebApp/WebApp/Features/Student/MyCourses/MyCoursesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Features.Student.MyCourses
+{
+    [Area("Student")]
+    public class MyCoursesController : Controller
+    {
+        [Route("/Student/Courses")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "My Courses";
+            return View("MyCourses");
+        }
+    }
+}
