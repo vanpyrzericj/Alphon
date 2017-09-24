201f180f81d71e8ec90e5fd5a99a382465ff8c77
Taiga <tonythetaiga@gmail.com>
2017-09-23 23:20:04 -0700

    Courses #14

---
+++ WebApp/WebApp/Features/Admin/Courses/CoursesController.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Features.Admin.Courses
+{
+    [Area("Admin")]
+    public class CoursesController : Controller
+    {
+        [Route("/Admin/Courses")]
+        public IActionResult Courses()
+        {
+            //For Prototyping: Just create an arbitrary list of Students (Accounts entity)
+            List<Course> model = new List<Course>
+            {
+                new Course
+                {
+                    name = "Software Engineering",
+                    number = "CSE442",
+                    Id = 1,
+                    description = "github101"
+                }
+            };
+
+            ViewData["Title"] = "Courses";
+            return View("Courses", model);
+        }
+
+        [Route("/Admin/Courses/{id}")]
+        public IActionResult Course(int id)
+        {
+            //For Prototyping: Just create an arbitrary student entity
+            var model = new Course
+            {
+				name = "Software Engineering",
+				number = "CSE442",
+				Id = 1,
+				description = "github101"
+            };
+
+            ViewData["Title"] = model.name;
+            return View("Course", model);
+        }
+
+        [HttpPost]
+        [Route("/Admin/Courses/Create")]
+        public IActionResult Create(Course course) => Redirect("/Admin/Courses");
+    }
+}
