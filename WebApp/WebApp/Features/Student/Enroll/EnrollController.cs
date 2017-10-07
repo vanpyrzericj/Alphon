8473d6312d67a83bc05f855b1cbb702c0df758a5
Eric J. Van Pyrz <Evanpyrz@jetmedia.us>
2017-10-06 20:59:40 -0400

    - Created Filter Class View Model - Created Test Filter Action Method for Debugging

--- WebApp/WebApp/Features/Student/Enroll/EnrollController.cs
+++ WebApp/WebApp/Features/Student/Enroll/EnrollController.cs
@@ -52,8 +52,14 @@ namespace WebApp.Features.Students.Enroll
             return View("Semester", model);
         }
 
+        /// <summary>
+        /// Page with filtered results
+        /// </summary>
+        /// <param name="SemesterID"></param>
+        /// <returns></returns>
+        [HttpPost]
         [Route("/Student/Enroll/{SemesterID}/Courses")]
-        public IActionResult CourseSelection(int SemesterID)
+        public IActionResult CourseSelection(int SemesterID, ClassFilterVM filter)
         {
             //For Prototyping: Just create an arbitrary list of Students (Accounts entity)
             List<Course> model = new List<Course>
@@ -120,7 +126,12 @@ namespace WebApp.Features.Students.Enroll
         {
             return View("Cart");
         }
-
+        
+        /// <summary>
+        /// This is the page that displays the filter form
+        /// </summary>
+        /// <param name="SemesterID"></param>
+        /// <returns></returns>
         [Route("/Student/Enroll/{SemesterID}/Search")]
         public IActionResult CourseSearch(int SemesterID)
         {
@@ -135,6 +146,39 @@ namespace WebApp.Features.Students.Enroll
             //For Prototyping: Just create an arbitrary list of Students (Accounts entity)
             ViewData["Title"] = "Checkout Result";
             return View("CheckoutResult");
+        }
+
+        //FOR TESTING PURPOSES
+        [Route("/Student/Enroll/{SemesterID}/TestCourses")]
+        public IActionResult TestCourseSelection(int SemesterID)
+        {
+            //TODO: Manipulate the filter object as needed
+            var filter = new ClassFilterVM();
+            filter.CourseNumber = 425;
+
+            //TODO: Delete this fake model and have your returned results be the model object.
+            List<Course> model = new List<Course>
+            {
+                new Course
+                {
+                    Id = 1,
+                    number = 442,
+                    name = "Software Engineering",
+                    description = "Industry standard software development."
+
+                },
+                 new Course
+                {
+                    Id = 2,
+                    number = 421,
+                    name = "Operating Systems",
+                    description = "Just resign now."
+
+                }
+            };
+
+            ViewData["Title"] = "Fall 2017";
+            return View("Courses", model);
         }
     }
 }
