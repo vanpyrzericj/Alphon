e8266254ba31d9912b632db7f83897b578912ae7
jf <jsfedyna@buffalo.edu>
2017-10-08 20:30:03 -0400

    Base MyCourses page table

--- WebApp/WebApp/Features/Student/MyCourses/MyCoursesController.cs
+++ WebApp/WebApp/Features/Student/MyCourses/MyCoursesController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebApp.Infrastructure.Inherits;
 using Microsoft.AspNetCore.Authorization;
+using WebApp.Models;
+using System.Security.Claims;
 
 namespace WebApp.Features.Student.MyCourses
 {
@@ -12,11 +14,40 @@ namespace WebApp.Features.Student.MyCourses
     [Authorize("Student")]
     public class MyCoursesController : Controller
     {
+
+        private HubContext _context;
+
+        public MyCoursesController()
+        {
+            _context = new HubContext();
+        }
+
+
         [Route("/Student/Courses")]
         public IActionResult Index()
         {
+            var identity = (ClaimsIdentity)User.Identity;
+            IEnumerable<Claim> claims = identity.Claims;
+            var val = claims.First(x => x.Type == "sub");
+            var acc = _context.Accounts.Find(Convert.ToInt32(val.Value));
+
+            var model = _context.Enrollments
+                        .Where(x => x.section.offering.semester.Id == 1)
+                        .Where(x => x.account.Id == acc.Id)
+                        .Select(x => new MyCoursesVM
+                        {
+                            professor = x.section.professor,
+                            room = x.section.room,
+                            timeslot = x.section.TimeSlots,
+                            credithours = x.section.offering.course.credithours,
+                            coursename = x.section.offering.course.name,
+                            coursenumber = x.section.offering.course.number,
+                            major = x.section.offering.course.major.name
+                        })
+                        .ToList();
+         
             ViewData["Title"] = "My Courses";
-            return View("MyCourses");
+            return View("MyCourses", model);
         }
     }
 }
---
+++ WebApp/WebApp/Features/Student/MyCourses/MyCoursesVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.Models;
+
+namespace WebApp.Features.Student.MyCourses
+{
+    public class MyCoursesVM
+    {
+        public IEnumerable<WebApp.Models.Semester> Semester { get; set; }
+
+        public int year { get; set; }
+
+        public int semesterid { get; set; }
+
+        public Professor professor { get; set; }
+
+        public String room { get; set; }
+
+        public int credithours { get; set; }
+
+        public ICollection<TimeSlot> timeslot { get; set; }
+
+        public int coursenumber { get; set; }
+
+        public int offeringid { get; set; }
+
+        public string coursename { get; set; }
+
+        public string major{ get; set; }
+    }
+}
