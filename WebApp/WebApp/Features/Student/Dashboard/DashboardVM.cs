b9cac23aae4891b73f90bd7f8d653d6dc6858f13
Eric J. Van Pyrz <ericvanp@buffalo.edu>
2017-10-08 22:52:39 -0400

    - Updated Student Dashboard

---
+++ WebApp/WebApp/Features/Student/Dashboard/DashboardVM.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using WebApp.Features.Student.MyCourses;
+using WebApp.Models;
+
+namespace WebApp.Features.Student.Dashboard
+{
+    public class DashboardVM
+    {
+        public int CurrentCourses { get; set; }
+        public int CurrentCreditHours { get; set; }
+        public int AvailableCreditHours { get; set; }
+        public int ShoppingCartCourses { get; set; }
+        public ICollection<MyCoursesVM> Enrollments { get; set; }
+        public Semester Semester { get; internal set; }
+    }
+}
